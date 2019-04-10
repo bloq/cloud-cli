7e054e34986a6c034d73792602a715e6af5c0b9b
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2019-04-10 16:08:31 -0300

    Refactor status view

--- src/commands/status.js
+++ src/commands/status.js
@@ -7,6 +7,7 @@ const request = require('request')
 const { promisify } = require('util')
 const { Command } = require('@oclif/command')
 const config = require('../config')
+require('console.table')
 
 const env = config.get('env') || 'prod'
 const services = config.get('services')
@@ -25,7 +26,7 @@ function getStatus (url) {
 
 class StatusCommand extends Command {
   async run () {
-    consola.info('Retrieving BloqCloud status')
+    consola.info(`Retrieving BloqCloud status: ${env}`)
 
     const spinner = ora().start()
     Promise.all([
@@ -34,23 +35,37 @@ class StatusCommand extends Command {
       getStatus(new URL(services[env].insight.btc.statusEndpoint, services[env].insight.btc.url)),
       getStatus(new URL(services[env].insight.bch.statusEndpoint, services[env].insight.bch.url))
     ])
-      .then(function (statuses) {
-        const [
-          isAccountsOk,
-          isNodesOk,
-          isInsightBTCOk,
-          isInsightBCHOk
-        ] = statuses
+      .then(function ([ isAccountsUp, isNodesUp, isInsightBtcUp, isInsightBchUp ]) {
+        const status = [
+          {
+            service: 'Accounts',
+            isUp: isAccountsUp ? '✔' : '❌',
+            url: services[env].accounts.url
+          },
+
+          {
+            service: 'Nodes',
+            isUp: isNodesUp ? '✔' : '❌',
+            url: services[env].nodes.url
+          },
+
+          {
+            service: 'Insight BTC',
+            isUp: isInsightBtcUp ? '✔' : '❌',
+            url: services[env].insight.btc.url
+          },
+
+          {
+            service: 'Insight BCH',
+            isUp: isInsightBchUp ? '✔' : '❌',
+            url: services[env].insight.btc.url
+          },
+        ]
 
         spinner.stop()
-        consola.info(
-          `BloqCloud Status:
-          * Accounts:     ${isAccountsOk ? '✔' : '❌'}
-          * Nodes:       ${isNodesOk ? '✔' : '❌'}
-          * Insight BTC: ${isInsightBTCOk ? '✔' : '❌'}
-          * Insight BCH: ${isInsightBCHOk ? '✔' : '❌'}
-          `
-        )
+
+        consola.info(`BloqCloud status: ${env}\n`)
+        console.table(status)
       })
       .catch(function (err) {
         consola.error(err)
