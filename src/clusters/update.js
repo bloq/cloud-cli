4981305ce899b981b17e037eb3f0eac3ca0b6a45
Gabriel Montes <gabriel@bloq.com>
2020-08-04 20:05:09 -0300

    Extend update cluster to handle services

--- src/clusters/update.js
+++ src/clusters/update.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const ora = require('ora')
 const consola = require('consola')
-const request = require('request')
+const fetch = require('node-fetch').default
 const inquirer = require('inquirer')
+const ora = require('ora')
 
 const config = require('../config')
 
@@ -15,6 +15,7 @@ const config = require('../config')
  * @param {Object} params.capacity Clusters total capacity
  * @param {Object} params.clusterId Cluster ID
  * @param {Object} params.onDemandCapacity Clusters on-demand capacity
+ * @param {Object} params.serviceId Service ID
  * @returns {Promise} The update cluster promise
  */
 async function updateCluster ({ accessToken, ...flags }) {
@@ -30,52 +31,64 @@ async function updateCluster ({ accessToken, ...flags }) {
     },
     {
       name: 'yes',
-      message: `You will update the cluster's capacity to ${flags.capacity} total and ${flags.onDemandCapacity} on-demand. Do you want to continue?`,
+      message: `You will update the cluster's ${
+        flags.serviceId ? `service to ${flags.serviceId} and ` : ''
+      }capacity to ${flags.capacity} total and ${
+        flags.onDemandCapacity
+      } on-demand. Do you want to continue?`,
       type: 'confirm',
       default: false,
       when: () => !flags.yes
     }
   ])
 
-  const { yes, clusterId, capacity, onDemandCapacity } = { ...flags, ...prompt }
+  const { yes, clusterId, capacity, onDemandCapacity, serviceId } = {
+    ...flags,
+    ...prompt
+  }
 
   if (!yes) {
-    return consola.error('Update canceled.')
+    return consola.error('Update canceled')
   }
 
   const env = config.get('env') || 'prod'
   const serviceUrl = config.get(`services.${env}.nodes.url`)
   const url = `${serviceUrl}/users/me/clusters/${clusterId}`
-  const Authorization = `Bearer ${accessToken}`
-  const json = { clusterId, capacity, onDemandCapacity }
+  const authorization = `Bearer ${accessToken}`
+  // const json = { clusterId, capacity, onDemandCapacity }
 
   const spinner = ora().start()
 
-  return request.patch(url, { headers: { Authorization }, json }, function (
-    err,
-    data
-  ) {
+  try {
+    const res = await fetch(url, {
+      method: 'patch',
+      headers: { authorization, 'content-type': 'application/json' },
+      body: JSON.stringify({ capacity, onDemandCapacity, serviceId })
+    })
+
+    if (!res.ok) {
+      const data = await res.json()
+
+      if (data.status === 401 || data.status === 403) {
+        return consola.error('Your session has expired')
+      }
+
+      if (data.status === 404) {
+        return consola.error('Cluster not found')
+      }
+
+      if (data.status !== 200) {
+        console.log(data)
+        return consola.error(`Error updating the cluster: ${data.title}`)
+      }
+    }
+
+    consola.success(`Cluster ${clusterId} updated successfully`)
+  } catch (err) {
+    consola.error(`Error updating the cluster: ${err.message}`)
+  } finally {
     spinner.stop()
-
-    if (err) {
-      return consola.error(`Error updating the cluster: ${err.message}`)
-    }
-
-    if (data.statusCode === 401 || data.statusCode === 403) {
-      return consola.error('Your session has expired')
-    }
-
-    if (data.statusCode === 404) {
-      return consola.error('Cluster not found')
-    }
-
-    if (data.statusCode !== 200) {
-      const body = JSON.parse(data.body)
-      return consola.error(`Error updating the cluster: ${body.code}`)
-    }
-
-    consola.success(`Cluster with ID ${clusterId} updated successfully`)
-  })
+  }
 }
 
 module.exports = updateCluster
