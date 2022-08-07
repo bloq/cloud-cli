e62e2bc90536c1c88fcc808106c31fc0552c6649
LeoSagan <leo@bloq.com>
2022-08-07 03:50:19 -0300

    Adds login & logout json output

--- src/commands/logout.js
+++ src/commands/logout.js
@@ -1,36 +1,53 @@
 'use strict'
 
 const consola = require('consola')
-const { Command } = require('@oclif/command')
+const { Command, flags } = require('@oclif/command')
 const inquirer = require('inquirer')
 const config = require('../config')
+const { formatResponse } = require('../utils')
 
 class LogoutCommand extends Command {
   async run() {
-    const user = config.get('user')
-    consola.info(
-      user ? `Clearing local data for ${user}` : 'Clearing local data'
-    )
+    const { flags } = this.parse(LogoutCommand)
+    const isJson = typeof flags.json !== 'undefined'
 
-    const { confirmation } = await inquirer.prompt([
+    const user = config.get('user')
+    !isJson &&
+      consola.info(
+        user ? `Clearing local data for ${user}` : 'Clearing local data'
+      )
+
+    const prompt = await inquirer.prompt([
       {
-        name: 'confirmation',
+        name: 'yes',
         message:
           'All your local data will be removed. Do you want to continue?',
-        type: 'confirm'
+        type: 'confirm',
+        when: () => !flags.json && !flags.yes
       }
     ])
 
-    if (!confirmation) {
-      consola.info('Logout aborted.')
+    const { yes } = {
+      ...prompt
+    }
+
+    if (!yes && !flags.yes && !flags.json) {
+      formatResponse(isJson, 'Logout aborted.')
       return
     }
 
-    consola.success('All your local data was removed')
+    formatResponse(isJson, 'Logout successful, all your local data was removed')
     config.clear()
   }
 }
 
 LogoutCommand.description = 'Clear local user data'
-
+LogoutCommand.flags = {
+  json: flags.boolean({ char: 'j', description: 'JSON output' }),
+  yes: flags.boolean({
+    char: 'y',
+    description: 'answer "yes" to prompts',
+    default: false
+  })
+}
 module.exports = LogoutCommand
